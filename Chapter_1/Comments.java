8ce24bc7e1ef95a4a653f206f96a27f59ba7bb51
JonnEmmanuelRenejane-UCM <jonnemmanuel96@gmail.com>
2025-09-16 08:45:14 +0800

    Add files via upload
    Added Activity 1-4

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,22 @@
+public class Comments {
+    public static void main(String[] args) {
+    
+        // This was written on September 16, 2025
+        
+        System.out.println("Example of a single-line comment.");
+
+        /* 
+           Tomorrow is Wednesday
+           It is Tuesday
+        */
+        
+        System.out.println("Example of a multi-line comment.");
+
+        /**
+         * This is a comment.
+         * This is only example.
+         */
+         
+        System.out.println("Example of a Javadoc comment.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,11 @@
+public class FavoriteSong {
+
+    public static void main(String[] args) {
+    
+        System.out.println("Heels, nails, blade, mascara");
+        System.out.println("Fit check for my napalm era");
+        System.out.println("Need to beat my face, make it cute and savage");
+        System.out.println("Mirror, mirror on my phone, who's the baddest? (Us, hello?)");
+        
+    }
+}
